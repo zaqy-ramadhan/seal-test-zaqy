76291dd2a049180d287f4a38831143026860618f
zaqy-ramadhan <ramadhanz619@gmail.com>
2024-08-14 16:44:59 +0700

    backend aja

--- frontend/src/components/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-// src/components/PrivateRoute.js
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ element, user }) => {
-    return user ? element : <Navigate to="/" />;
-};
-
-export default PrivateRoute;
